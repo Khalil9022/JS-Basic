35632070e720cd086dc1f1db84b64088eecc743b
Khalil Attalla Firdia <khalilattalla2209@gmail.com>
2022-09-01 13:24:21 +0700

    Create folder obejects and the other file

---
+++ Objects/index.js
@@ -0,0 +1,48 @@
+
+//contoh object : 
+const circle = {
+    radius : 1,
+    location : {
+        x : 1,
+        y : 1
+    },
+    isVisible : true,
+    draw : function() {
+        console.log('draw');
+    }
+};
+
+// circle.color = "yellow"
+// console.log(circle);
+// delete circle.color
+// console.log(circle);
+
+//circle.draw()
+
+// Factory Function
+function createCircle(radius){
+    return {
+        radius,
+        draw(){
+            console.log('draw');
+        },
+    };
+}
+
+// const circle1 = createCircle(1)
+// console.log(circle1);
+
+// const circle2 = createCircle(2)
+// console.log(circle2);
+
+//constructor function
+function Circle(radius){
+    this.radius = radius
+    this.draw = function(){
+        console.log('draw');
+    }
+    return this
+}
+
+// const circle3 = new Circle(1)
+// console.log(circle3);
